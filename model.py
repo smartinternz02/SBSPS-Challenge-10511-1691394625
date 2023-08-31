1a75010b7427a93e4df027b9d3169d269a5ab929
K NAGA HIMAJA <himajakeela18@gmail.com>
2023-08-31 22:56:55 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,10 +6,8 @@ cropdf = pd.read_csv("crop_recommendation.csv")
 crop_summary = pd.pivot_table(cropdf,index=['label'],aggfunc='mean')
 X = cropdf.drop('label', axis=1)
 y = cropdf['label']
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, shuffle = True, random_state = 0)
 
 model = lgb.LGBMClassifier()
-model.fit(X_train, y_train)
+model.fit(X, y)
 
 pickle.dump(model, open('save_model.pkl','wb'))
